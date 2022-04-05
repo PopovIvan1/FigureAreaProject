f93ca767e0335d30ba28b0ab9c5777fa51163ef9
Popov Ivan <bottob79@mail.ru>
2022-04-05 12:55:36 +0300

    feat: added initial project structure

---
+++ FigureAreaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FigureAreaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FigureAreaProject
+
